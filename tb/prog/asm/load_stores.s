4f1ac8b6f6c24fb5f6ec4ca6fb558a106954dc60
parkwilliamy <parkwilliamy@gmail.com>
2025-11-25 21:46:12 -0500

    wrote ls test

---
+++ tb/prog/asm/load_stores.s
@@ -0,0 +1,19 @@
+    .globl _start
+
+    .section .text
+_start:
+    
+    la  x5, my_data         # x5 = base address of .data section (0x5000)
+    li x1, 2                # x1 = 2
+    sw x1, 0(x5)            # mem[0x5000] = x1
+    lw x3, 0(x5)            # x3 = mem[0x5000]
+    
+    
+    .section .data
+my_data:
+    .word 0                 # reserve 4 bytes for x4
+
+
+
+
+
