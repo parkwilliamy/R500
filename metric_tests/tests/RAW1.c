2bff1a6feefe92166ffcedbdada16b50d16d000e
parkwilliamy <parkwilliamy@gmail.com>
2025-12-21 16:59:12 -0800

    debugging raw

--- metric_tests/tests/RAW1.c
+++ metric_tests/tests/RAW1.c
@@ -1,6 +1,6 @@
 int main() {
 
-    int a,b,c,d,e,f,g,h;
+    volatile int a,b,c,d,e,f,g,h;
 
     a = 20;
     b = a ^ 1;
@@ -13,14 +13,14 @@ int main() {
 
     g = 2;
     h = g | 0;
-    
+    /*
     volatile int* RESULT_ADDR = (volatile int*)0x6000;
 
     RESULT_ADDR[0] = b;
     RESULT_ADDR[1] = d;
     RESULT_ADDR[2] = f;
     RESULT_ADDR[3] = h;
-
+    */
     while(1);
 
 }
